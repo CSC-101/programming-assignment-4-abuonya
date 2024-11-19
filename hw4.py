bed5877737f547c68e71f2baf3b6993ba42cc1fe
annalisebuonya <abuonya@calpoly.edu>
2024-11-19 09:50:40 -0800

    commit

--- hw4.py
+++ hw4.py
@@ -3,18 +3,20 @@ from data import CountyDemographics
 import build_data
 import argparse
 
-counties = build_data.get_data()
+
 
 
 # make pretty later ************************
-def display():
+def display(counties):
     for county in counties:
         print(f"{county.county} {county.age} {county.education} {county.ethnicities} {county.income} {county.state}")
 
-def filter_state():
+def filter_state(counties, state):
+    filtered_by_state = []
     for county in counties:
-        if county.state == sys.argv[1]:
-            print(county)
+        if county.state == state:
+            filtered_by_state.append(county)
+    return filtered_by_state
 
 def all_operations():
     try:
@@ -23,10 +25,20 @@ def all_operations():
         print("Could not access the file provided.")
         sys.exit(1)
 
-    with open(given_operation, "r"):
-        for line in
+    with open(given_operation, "r") as temporary:
+        operations = temporary.readlines()
+
+    counties = build_data.get_data()
+
+    for operation in operations:
+        if "display" in operation:
+            display(counties)
+
+        elif "filter-state" in operation:
+            component = operation.split(":")[1]
+            filter_state(counties, component)
+            display(counties)
 
 
 if __name__ == "__main__":
-    given_operation = sys.argv[1]
-    display()
+    all_operations()
