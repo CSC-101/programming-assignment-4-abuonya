ea14438ed279bcdb47542bf806f535d3780f3d1e
annalisebuonya <abuonya@calpoly.edu>
2024-11-18 22:24:18 -0800

    commit

--- hw4.py
+++ hw4.py
@@ -5,10 +5,19 @@ import argparse
 
 counties = build_data.get_data()
 
+
+# make pretty later ************************
 def display():
     for county in counties:
         print(f"{county.county} {county.age} {county.education} {county.ethnicities} {county.income} {county.state}")
 
+def filter_state():
+    for county in counties:
+        if county.state == sys.argv[1]:
+            print(county)
+
+def all_operations(given_operation)
 
 if __name__ == "__main__":
+    given_operation = sys.argv[1]
     display()
